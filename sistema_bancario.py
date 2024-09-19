a500843a5015660a0a859e83c778012cf29fdda4
Italo Rodrigues <italorbezerra1@gmail.com>
2024-09-19 17:04:50 -0300

    sistema_bancario.py

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -1,5 +1,9 @@
 from datetime import datetime
 
+class Usuario:
+    def __init__(self, cpf):
+        self.cpf = cpf
+        self.conta = Banco()
 
 class Banco:
     def __init__(self):
@@ -20,7 +24,6 @@ class Banco:
 
     def saque(self, valor):
         hoje = datetime.now().date()
-
 
         if self.data_ultimo_saque != hoje:
             self.data_ultimo_saque = hoje
@@ -52,33 +55,61 @@ class Banco:
             print(f'  - {transacao}')
 
 
+def criar_usuario():
+    cpf = input("Digite o CPF do usuário: ")
+    return Usuario(cpf)
 
-banco = Banco()
+
+usuarios = []
 
 while True:
     print('\nBem-vindo ao sistema bancário!')
-    print('1. Depositar')
-    print('2. Sacar')
-    print('3. Mostrar Extrato')
-    print('4. Sair')
+    print('1. Criar Usuário')
+    print('2. Acessar Conta')
+    print('3. Sair')
 
     opcao = input('Escolha uma opção: ')
 
     if opcao == '1':
-        try:
-            valor = float(input("Digite o valor do depósito: "))
-            banco.deposito(valor)
-        except ValueError:
-            print("Valor inválido. Digite um número.")
+        usuario = criar_usuario()
+        usuarios.append(usuario)
+        print(f'Usuário com CPF {usuario.cpf} criado com sucesso!')
     elif opcao == '2':
-        try:
-            valor = float(input('Digite o valor do saque: '))
-            banco.saque(valor)
-        except ValueError:
-            print("Valor inválido. Digite um número.")
+        cpf = input("Digite o CPF do usuário para acessar a conta: ")
+        usuario_encontrado = next((u for u in usuarios if u.cpf == cpf), None)
+
+        if usuario_encontrado:
+            while True:
+                print('\nAcesso à Conta:')
+                print('1. Depositar')
+                print('2. Sacar')
+                print('3. Mostrar Extrato')
+                print('4. Sair')
+
+                opcao_conta = input('Escolha uma opção: ')
+
+                if opcao_conta == '1':
+                    try:
+                        valor = float(input("Digite o valor do depósito: "))
+                        usuario_encontrado.conta.deposito(valor)
+                    except ValueError:
+                        print("Valor inválido. Digite um número.")
+                elif opcao_conta == '2':
+                    try:
+                        valor = float(input('Digite o valor do saque: '))
+                        usuario_encontrado.conta.saque(valor)
+                    except ValueError:
+                        print("Valor inválido. Digite um número.")
+                elif opcao_conta == '3':
+                    usuario_encontrado.conta.mostrar_extrato()
+                elif opcao_conta == '4':
+                    print('Saindo do sistema...')
+                    break
+                else:
+                    print('Operação inválida. Tente novamente.')
+        else:
+            print("Usuário não encontrado.")
     elif opcao == '3':
-        banco.mostrar_extrato()
-    elif opcao == '4':
         print('Saindo do sistema...')
         break
     else:
